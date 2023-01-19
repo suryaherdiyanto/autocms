7ad71f7c76a5de17855cea6ad47cd951c52af942
Surya Herdiyanto <surya@apwin.com>
2023-01-19 12:08:42 +0800

    adding deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+
+echo "Pulling from https://github.com/suryaherdiyanto/autocms"
+git pull --no-ff
+
+echo "Building...."
+docker-compose build
+
+echo "Restarting...."
+docker-compose stop
+docker-compose up -d
+
+echo "Done."
