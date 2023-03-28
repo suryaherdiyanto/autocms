5fe77cff6e9aac8bb95520a899838e5429e64367
Surya <suryaherdiyanto@gmail.com>
2023-03-28 22:36:09 +0800

    sligh tune on the dashboard cards

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -13,12 +13,12 @@
                     <div class="w-1/4 mr-5">
                         <div class="bg-white rounded-lg border border-gray-200 w-full p-5">
                             <div class="flex mb-4">
-                                <div class="flex w-12 h-12 justify-center items-center mr-3">
+                                <div class="flex w-20 h-20 justify-center items-center mr-3">
                                     <i class="fa-solid fa-paste text-accent-primary text-4xl"></i>
                                 </div>
                                 <div class="flex flex-col justify-center">
-                                    <h3 class="font-cairo font-bold text-3xl">12</h3>
-                                    <p class="text-gray-500 text-xs">Posts published</p>
+                                    <h3 class="font-cairo font-bold text-4xl">12</h3>
+                                    <p class="text-gray-500 text-xs my-2">Posts published</p>
                                 </div>
                             </div>
                             <a href="#" class="py-1.5 px-2 rounded bg-accent-primary text-white text-xs font-semibold hover:bg-accent-primary-darker">
@@ -29,12 +29,12 @@
                     <div class="w-1/4 mr-5">
                         <div class="bg-white rounded-lg border border-gray-200 w-full p-5">
                             <div class="flex mb-4">
-                                <div class="flex w-12 h-12 justify-center items-center mr-3">
+                                <div class="flex w-20 h-20 justify-center items-center mr-3">
                                     <i class="fa-solid fa-envelope text-accent-primary text-4xl"></i>
                                 </div>
                                 <div class="flex flex-col justify-center">
-                                    <h3 class="font-cairo font-bold text-3xl">13</h3>
-                                    <p class="text-gray-500 text-xs">Example Widget</p>
+                                    <h3 class="font-cairo font-bold text-4xl">13</h3>
+                                    <p class="text-gray-500 text-xs my-2">Example Widget</p>
                                 </div>
                             </div>
                             <a href="#" class="py-1.5 px-2 rounded bg-accent-primary text-white text-xs font-semibold hover:bg-accent-primary-darker">
@@ -45,12 +45,12 @@
                     <div class="w-1/4 mr-5">
                         <div class="bg-white rounded-lg border border-gray-200 w-full p-5">
                             <div class="flex mb-4">
-                                <div class="flex w-12 h-12 justify-center items-center mr-3">
+                                <div class="flex w-20 h-20 justify-center items-center mr-3">
                                     <i class="fa-solid fa-comment text-accent-primary text-4xl"></i>
                                 </div>
                                 <div class="flex flex-col justify-center">
-                                    <h3 class="font-cairo font-bold text-3xl">13</h3>
-                                    <p class="text-gray-500 text-xs">Example Widget</p>
+                                    <h3 class="font-cairo font-bold text-4xl">13</h3>
+                                    <p class="text-gray-500 text-xs my-2">Example Widget</p>
                                 </div>
                             </div>
                             <a href="#" class="py-1.5 px-2 rounded bg-accent-primary text-white text-xs font-semibold hover:bg-accent-primary-darker">
@@ -61,12 +61,12 @@
                     <div class="w-1/4 mr-5">
                         <div class="bg-white rounded-lg border border-gray-200 w-full p-5">
                             <div class="flex mb-4">
-                                <div class="flex w-12 h-12 justify-center items-center mr-3">
+                                <div class="flex w-20 h-20 justify-center items-center mr-3">
                                     <i class="fa-solid fa-envelope text-accent-primary text-4xl"></i>
                                 </div>
                                 <div class="flex flex-col justify-center">
-                                    <h3 class="font-cairo font-bold text-3xl">13</h3>
-                                    <p class="text-gray-500 text-xs">Example Widget</p>
+                                    <h3 class="font-cairo font-bold text-4xl">13</h3>
+                                    <p class="text-gray-500 text-xs my-2">Example Widget</p>
                                 </div>
                             </div>
                             <a href="#" class="py-1.5 px-2 rounded bg-accent-primary text-white text-xs font-semibold hover:bg-accent-primary-darker">
