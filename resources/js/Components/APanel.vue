ab1a327da788f9025ce912c97cba64ba29c78ff4
Surya Herdiyanto <surya@apwin.com>
2023-03-30 09:05:25 +0800

    improve style for the panel

--- resources/js/Components/APanel.vue
+++ resources/js/Components/APanel.vue
@@ -1,12 +1,12 @@
 <template>
     <a-box is-panel>
-        <div class="w-full bg-gray-300 py-4 px-5 rounded-t-lg">
-            <h3 class="font-cairo text-xl font-bold">{{ title }}</h3>
+        <div class="w-full bg-gray-100 py-4 px-5 rounded-t-lg">
+            <h3 class="font-cairo text-lg font-bold text-gray-800">{{ title }}</h3>
         </div>
-        <div class="p-5 w-full">
+        <div class="py-4 px-5 w-full">
             <slot name="content"></slot>
         </div>
-        <div v-if="hasFooter" class="py-4 px-5 w-full bg-gray-300 rounded-b-lg">
+        <div v-if="hasFooter" class="py-4 px-5 w-full bg-gray-100 rounded-b-lg">
             <slot name="footer"></slot>
         </div>
     </a-box>
