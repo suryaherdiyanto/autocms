7b1ef087ffd34d93fc2d03a959e012a94302e78b
Surya Herdiyanto <surya@apwin.com>
2023-04-01 22:14:29 +0800

    adding table loading and table loading spinner

--- resources/js/Components/ATable.vue
+++ resources/js/Components/ATable.vue
@@ -1,6 +1,7 @@
 <template>
-    <table class="w-full table-auto">
-        <table class="w-full table-auto">
+    <div class="relative h-32">
+        <table-loading></table-loading>
+        <table class="w-full h-32 table-auto">
             <thead class="text-sm font-semibold border-y border-y-gray-200">
                 <tr>
                     <td v-for="(heading,index) in table.headings" :key="index" class="py-2">
@@ -14,10 +15,11 @@
                 </tr>
             </tbody>
         </table>
-    </table>
+    </div>
 </template>
 
 <script>
+import TableLoading from './Table/TableLoading.vue';
 import { reactive } from 'vue';
 
 export default {
@@ -35,6 +37,9 @@ export default {
                 return [];
             }
         }
+    },
+    components: {
+        TableLoading
     },
     setup(props) {
         const table = reactive({
---
+++ resources/js/Components/Table/TableLoading.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="w-full h-32 bg-gray-300/60 absolute z-10">
+        <div class="p-4 bg-primary absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded">
+            <table-loading-spinner></table-loading-spinner>
+            <span class="text-white text-sm font-semibold">Please wait...</span>
+        </div>
+    </div>
+</template>
+
+<script>
+import TableLoadingSpinner from './TableLoadingSpinner.vue';
+
+export default {
+    components: {
+        TableLoadingSpinner
+    },
+    setup() {
+
+    },
+}
+</script>
