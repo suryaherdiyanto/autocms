a9dd51e29a59f1e922a9529eaca7cb3fd4140132
Surya <suryaherdiyanto@gmail.com>
2023-04-15 20:35:02 +0800

    remove default href attribute from AButton component

--- resources/js/Components/AButton.vue
+++ resources/js/Components/AButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <a href="#" :class="{'pointer-events-none': loading, 'bg-primary-lighter': loading}" class="py-1.5 px-2 rounded bg-primary text-gray-200 text-xs font-semibold hover:bg-primary-lighter">
+    <a :class="{'pointer-events-none': loading, 'bg-primary-lighter': loading}" class="py-1.5 px-2 rounded bg-primary text-gray-200 text-xs font-semibold hover:bg-primary-lighter">
         <slot v-if="!loading"></slot>
         <slot v-if="loading" name="loading">
             <span>Loading...</span> <button-loading-spinner v-if="loading"></button-loading-spinner>
