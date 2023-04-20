a23a910eca86e71782871803a7efdb57d66c9d99
Surya <suryaherdiyanto@gmail.com>
2023-04-20 16:04:44 +0800

    adding profile menu

--- resources/js/Templates/TheHeader.vue
+++ resources/js/Templates/TheHeader.vue
@@ -15,13 +15,18 @@
                     <span class="w-5 h-5 flex justify-center items-center font-cairo font-bold bg-sky-600 text-black rounded-full text-xs absolute top-0 right-0">5</span>
                 </span>
             </div>
-            <div class="px-2 py-6 flex items-center cursor-pointer hover:bg-primary-lighter">
+            <div class="px-2 py-6 flex items-center cursor-pointer hover:bg-primary-lighter relative group">
                 <img width="32" height="32" src="../../images/blank-profile-picture.png" alt="Profile Pic" class="rounded mr-3">
                 <div class="flex flex-col mr-5">
                     <span class="font-cairo text-white font-semibold text-sm">Surya Herdiyanto</span>
                     <span class="font-cairo text-gray-500 font-semibold text-xs">Administrator</span>
                 </div>
                 <i class="fa-solid fa-caret-down text-accent-primary"></i>
+
+                <dib class="absolute w-full bg-white border rounded border-gray-200 left-0 top-full transition-all duration-200 translate-y-6 opacity-30 invisible group-hover:visible group-hover:translate-y-0 group-hover:opacity-100">
+                    <a class="block p-4 text-sm border-b border-b-gray-200 hover:bg-gray-100" href="#"><i class="fas fa-user"></i>&nbsp; Profile</a>
+                    <a class="block p-4 text-sm border-b-gray-200 hover:bg-gray-100" href="#"><i class="fas fa-power-off"></i>&nbsp; Logout</a>
+                </dib>
             </div>
         </div>
     </div>
