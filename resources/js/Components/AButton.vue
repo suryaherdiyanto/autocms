fd9eb6e1e3a73111d96ff5b85d8a93e55d0c6308
Surya <suryaherdiyanto@gmail.com>
2023-04-20 15:29:39 +0800

    dynamic button styles

--- resources/js/Components/AButton.vue
+++ resources/js/Components/AButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <a :class="{'pointer-events-none': loading, 'bg-primary-lighter': loading}" class="py-1.5 px-2 rounded bg-primary text-gray-200 text-xs font-semibold cursor-pointer hover:bg-primary-lighter">
+    <a :class="styles" class="py-1.5 px-2 rounded text-gray-200 text-xs font-semibold cursor-pointer hover:bg-primary-lighter">
         <slot v-if="!loading"></slot>
         <slot v-if="loading" name="loading">
             <span>Loading...</span> <button-loading-spinner v-if="loading"></button-loading-spinner>
@@ -8,6 +8,7 @@
 </template>
 
 <script>
+import { reactive, ReactiveEffect } from 'vue';
 import ButtonLoadingSpinner from './Button/ButtonLoadingSpinner.vue';
 
 export default {
@@ -20,8 +21,10 @@ export default {
     components: {
         ButtonLoadingSpinner
     },
-    setup() {
+    setup(props) {
+        let styles = reactive({'pointer-events-none': props.loading, 'bg-primary-lighter': props.loading, 'bg-primary': !props.loading});
 
+        return { styles }
     },
 }
 </script>
