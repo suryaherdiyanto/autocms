0e5d6099f8263fae2b81db48113d959554744273
Surya <suryaherdiyanto@gmail.com>
2023-04-23 12:31:09 +0800

    change button bg for loading state

--- resources/js/Components/AButton.vue
+++ resources/js/Components/AButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <a :class="styles" class="py-1.5 px-2 rounded text-gray-200 text-xs font-semibold cursor-pointer hover:bg-primary-lighter">
+    <a :class="styles" class="bg-primary py-1.5 px-2 rounded text-gray-200 text-xs font-semibold cursor-pointer hover:bg-primary-lighter">
         <slot v-if="!loading"></slot>
         <slot v-if="loading" name="loading">
             <span>Loading...</span> <button-loading-spinner v-if="loading"></button-loading-spinner>
@@ -22,7 +22,7 @@ export default {
         ButtonLoadingSpinner
     },
     setup(props) {
-        let styles = reactive({'pointer-events-none': props.loading, 'bg-primary-lighter': props.loading, 'bg-primary': !props.loading});
+        let styles = reactive({'pointer-events-none': props.loading});
 
         return { styles }
     },
