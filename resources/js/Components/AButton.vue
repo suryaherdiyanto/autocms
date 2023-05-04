4302acaa24c45520e3812a9d100f617cdd40fc2f
Surya Herdiyanto <surya@apwin.com>
2023-05-04 22:50:06 +0800

    setup for typescript

--- resources/js/Components/AButton.vue
+++ resources/js/Components/AButton.vue
@@ -1,3 +1,14 @@
+<script setup lang="ts">
+import { reactive, defineProps } from 'vue';
+import ButtonLoadingSpinner from './Button/ButtonLoadingSpinner.vue';
+
+const props = defineProps({
+    loading: Boolean
+});
+
+let styles = reactive({'pointer-events-none': props.loading});
+</script>
+
 <template>
     <a :class="styles" class="bg-primary py-1.5 px-2 rounded text-gray-200 text-xs font-semibold cursor-pointer hover:bg-primary-lighter">
         <slot v-if="!loading"></slot>
@@ -6,25 +17,3 @@
         </slot>
     </a>
 </template>
-
-<script>
-import { reactive, ReactiveEffect } from 'vue';
-import ButtonLoadingSpinner from './Button/ButtonLoadingSpinner.vue';
-
-export default {
-    props: {
-        loading: {
-            type: Boolean,
-            default: false
-        }
-    },
-    components: {
-        ButtonLoadingSpinner
-    },
-    setup(props) {
-        let styles = reactive({'pointer-events-none': props.loading});
-
-        return { styles }
-    },
-}
-</script>
---
+++ resources/js/app.ts
@@ -0,0 +1,42 @@
+import { createApp, h, DefineComponent } from 'vue';
+import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+
+import AButton from '@/Components/AButton.vue';
+import ABox from '@/Components/ABox.vue';
+import APanel from '@/Components/APanel.vue';
+import ATable from '@/Components/ATable.vue';
+import AInput from '@/Components/Form/AInput.vue';
+import ATextarea from '@/Components/Form/ATextarea.vue';
+import ARadioGroup from '@/Components/Form/ARadioGroup.vue';
+import ACheckbox from '@/Components/Form/ACheckbox.vue';
+import ACheckboxGroup from '@/Components/Form/ACheckboxGroup.vue';
+import ASelect from '@/Components/Form/ASelect.vue';
+import AEditor from '@/Components/Form/AEditor.vue';
+import ARow from '@/Components/ARow.vue';
+import AAlert from '@/Components/AAlert.vue';
+
+createInertiaApp({
+    id: 'app',
+    resolve: (name: string) => {
+      return resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob<DefineComponent>('./Pages/**/*.vue'));
+    },
+    setup({ el, App, props, plugin }) {
+      createApp({ render: () => h(App, props) })
+        .use(plugin)
+        .component('AButton', AButton)
+        .component('ABox', ABox)
+        .component('APanel', APanel)
+        .component('ATable', ATable)
+        .component('AInput', AInput)
+        .component('ATextarea', ATextarea)
+        .component('ARadioGroup', ARadioGroup)
+        .component('ACheckbox', ACheckbox)
+        .component('ACheckboxGroup', ACheckboxGroup)
+        .component('ASelect', ASelect)
+        .component('AEditor', AEditor)
+        .component('ARow', ARow)
+        .component('AAlert', AAlert)
+        .mount(el)
+    },
+  })
