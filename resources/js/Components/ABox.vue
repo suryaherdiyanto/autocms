b24d5a3b48a3edf320bcbd7f101fcf202f04d728
Surya <suryaherdiyanto@gmail.com>
2023-05-07 15:08:25 +0800

    convert ABox.vue component into typescript

--- resources/js/Components/ABox.vue
+++ resources/js/Components/ABox.vue
@@ -1,29 +1,23 @@
+<script setup lang="ts">
+import { reactive, defineProps } from 'vue';
+
+interface Box {
+    isPanel: Boolean
+}
+
+const props = defineProps<Box>();
+
+const atts = reactive<Object>({
+    'p-5': true,
+});
+
+if (props.isPanel) {
+    atts['p-5'] = false;
+}
+</script>
+
 <template>
     <div class="bg-white rounded-lg border border-gray-200 w-full" :class="atts">
         <slot></slot>
     </div>
 </template>
-
-<script>
-import { reactive } from 'vue';
-
-export default {
-    props: {
-        isPanel: {
-            type: Boolean,
-            default: false
-        },
-    },
-    setup(props) {
-        const atts = reactive({
-            'p-5': true,
-        });
-
-        if (props.isPanel) {
-            atts['p-5'] = false;
-        }
-
-        return { atts };
-    },
-}
-</script>
