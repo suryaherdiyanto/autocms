234ad1dc521f78b3d21757f3f5ee42a59fd7823b
Surya <suryaherdiyanto@gmail.com>
2023-05-07 16:08:07 +0800

    Adjust TableLoading & TableLoadingSpinner

--- resources/js/Components/Table/TableLoadingSpinner.vue
+++ resources/js/Components/Table/TableLoadingSpinner.vue
@@ -1,19 +1,10 @@
+<script setup lang="ts">
+import LoadingSpinner from '@/Components/LoadingSpinner.vue';
+</script>
+
 <template>
     <div role="status" class="flex justify-center">
         <loading-spinner></loading-spinner>
         <span class="sr-only">Loading...</span>
     </div>
 </template>
-
-<script>
-import LoadingSpinner from '../LoadingSpinner.vue';
-
-export default {
-    components: {
-        LoadingSpinner
-    },
-    setup() {
-
-    },
-}
-</script>
