3ca950eaff6f92f2ddffb7d27b693ce233213eee
Surya Herdiyanto <surya@apwin.com>
2023-05-13 21:47:57 +0800

    cleanup plugins.ts and move the notification logic into notification.ts

--- resources/js/Components/Plugins/notification.ts
+++ resources/js/Components/Plugins/notification.ts
@@ -1,5 +1,31 @@
+import NotificationTemplate from "./Notification.vue";
+import { render,h } from "vue";
+
 export interface NotificationProps {
     title?: string,
     message: string,
     offset?: number
+}
+
+export function notify(options: NotificationProps) {
+    let template = h(NotificationTemplate, options);
+    const notifyDOM = document.querySelectorAll<HTMLElement>('.a-notification');
+
+    if (notifyDOM.length > 0) {
+
+        template.props.offset = (notifyDOM.length * notifyDOM[0].offsetHeight) + (16 * (notifyDOM.length+1));
+    }
+
+    const container = document.body as HTMLTemplateElement;
+    const el = document.createElement('div');
+
+    render(template, el)
+
+    container.appendChild(el.firstChild);
+    this.notificationCount += 1;
+
+    return () => {
+        render(null, el);
+        template = null;
+    }
 }
--- resources/js/plugins.ts
+++ resources/js/plugins.ts
@@ -1,34 +1,12 @@
-import { App, render, h } from "vue";
-import NotificationTemplate from "@/Components/Plugins/Notification.vue";
-import { NotificationProps } from "@/Components/Plugins/notification";
+import { App } from "vue";
+import { notify } from "./Components/Plugins/notification";
 
 export const Notification = {
     install(app: App) {
         const $notify = {
-            show(options: NotificationProps) {
-
-                let template = h(NotificationTemplate, options);
-                const notifyDOM = document.querySelectorAll<HTMLElement>('.a-notification');
-
-                if (notifyDOM.length > 0) {
-
-                    template.props.offset = (notifyDOM.length * notifyDOM[0].offsetHeight) + (16 * (notifyDOM.length+1));
-                }
-
-                const container = document.body as HTMLTemplateElement;
-                const el = document.createElement('div');
-
-                render(template, el)
-
-                container.appendChild(el.firstChild);
-                this.notificationCount += 1;
-
-                return () => {
-                    render(null, el);
-                    template = null;
-                }
-            }
+            show: notify
         }
+
         app.provide('anotification', $notify);
     }
 }
