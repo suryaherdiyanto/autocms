fc35045999ca7775174bf610ec16c8b85cf7b09f
Surya Herdiyanto <surya@apwin.com>
2023-05-13 17:06:03 +0800

    fixed issue Notification props

--- resources/js/plugins.ts
+++ resources/js/plugins.ts
@@ -1,22 +1,24 @@
-import { App, createVNode, render, defineProps, withDefaults, provide } from "vue";
+import { App, render, h } from "vue";
 import NotificationTemplate from "@/Components/Plugins/Notification.vue";
 import { NotificationProps } from "@/Components/Plugins/notification";
 
 export const Notification = {
     install(app: App) {
         const $notify = {
-            show(options: NotificationProps): void {
-                const props = withDefaults(defineProps<NotificationProps>(), {
-                    title: options.title,
-                    message: options.message
-                });
+            show(options: NotificationProps) {
 
-                const template = createVNode(NotificationTemplate, props);
+                let template = h(NotificationTemplate, options);
                 const container = document.body as HTMLTemplateElement;
                 const el = document.createElement('div');
 
-                render(template, el);
-                container.appendChild(el.firstElementChild);
+                render(template, el)
+
+                container.appendChild(el.firstChild);
+
+                return () => {
+                    render(null, el);
+                    template = null;
+                }
             }
         }
         app.provide('anotification', $notify);
