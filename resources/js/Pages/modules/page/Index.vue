de75cc3b42237419d19e8cd0f85c396dd5bd9a13
suryaherdiyanto <suryaherdiyanto@gmail.com>
2023-12-03 00:29:25 +0800

    add Page Index.vue & link the sidebar

--- resources/js/Pages/modules/page/Index.vue
+++ resources/js/Pages/modules/page/Index.vue
@@ -1,3 +1,8 @@
-<div>
-
-</div>
+<template>
+    <div class="p-6">
+        <div class="py-4">
+            <h2 class="font-cairo text-2xl font-bold">Page</h2>
+            <p class="text-gray-500 text-sm">Page data management.</p>
+        </div>
+    </div>
+</template>
--- resources/js/Templates/TheSidebar.vue
+++ resources/js/Templates/TheSidebar.vue
@@ -1,3 +1,6 @@
+<script setup lang="ts">
+import { Link } from "@inertiajs/vue3";
+</script>
 <template>
     <aside class="min-h-screen h-full w-full p-6 bg-primary overflow-x-auto">
         <div class="flex pb-6 align-middle justify-center">
@@ -5,26 +8,18 @@
         </div>
         <h2 class="text-gray-400 pb-4 uppercase text-sm font-semibold">main menu</h2>
         <div class="flex flex-col">
-            <a href="#" class="py-4 px-2 text-gray-400 text-sm font-semibold font-cairo hover:bg-primary-lighter hover:rounded">
+            <Link href="/admin/dashboard" class="py-4 px-2 text-gray-400 text-sm font-semibold font-cairo hover:bg-primary-lighter hover:rounded">
                <i class="fa-solid fa-gauge-high mr-1"></i> Dashboard
-            </a>
+            </Link>
             <a href="#" class="py-4 px-2 text-gray-400 text-sm font-semibold font-cairo hover:bg-primary-lighter hover:rounded">
                 <i class="fa-regular fa-images mr-1"></i> Media
             </a>
-            <a href="#" class="py-4 px-2 text-gray-400 text-sm font-semibold font-cairo hover:bg-primary-lighter hover:rounded">
+            <Link href="/admin/pages" class="py-4 px-2 text-gray-400 text-sm font-semibold font-cairo hover:bg-primary-lighter hover:rounded">
                 <i class="fa-regular fa-file mr-1"></i> Pages
-            </a>
+            </Link>
             <a href="#" class="py-4 px-2 text-gray-400 text-sm font-semibold font-cairo hover:bg-primary-lighter hover:rounded">
                 <i class="fa-solid fa-gear mr-1"></i> Settings
             </a>
         </div>
     </aside>
 </template>
-
-<script>
-export default {
-    setup() {
-
-    },
-}
-</script>
