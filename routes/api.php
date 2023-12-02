6d267a50fd1b1f396daaad54ec7b2916537e6f76
suryaherdiyanto <suryaherdiyanto@gmail.com>
2023-12-03 00:07:31 +0800

    remove routes

--- routes/api.php
+++ routes/api.php
@@ -18,8 +18,3 @@ use App\Http\Controllers\Api\PageController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::middleware('auth:sanctum')->group(function() {
-
-    Route::resource('pages', PageController::class);
-});
